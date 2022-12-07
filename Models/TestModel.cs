80708ccc1cfde85f3f3cea20863eee8c359f76d7
aeongt710 <unknowndonlion@gmail.com>
2022-12-07 16:45:57 +0500

    Add project files.

---
+++ Models/TestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    public class TestModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Time { get; set; } = DateTime.Now;
+    }
+}
